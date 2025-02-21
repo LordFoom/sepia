2f1c22082d088ee99f82581d9f689f2dcbbaecdd
Timothy Spring <foom@amazon.com>
2025-02-21 18:07:50 +0200

    CLI arg stubs

--- src/args.rs
+++ src/args.rs
@@ -1,2 +1,8 @@
-
-
+use clap::{Args, Parser};
+#[derive(Debug, Parser)]
+#[command(version = "0.1", about = "Screenshottig app to record  my days")]
+pub struct AppArgs {
+    ///Time in seconds between screenshots
+    #[arg(short, long)]
+    time: u8,
+}
