a8a3381ca3c2d321bfacfb2cb3c9076f76e1f26e
Timothy Spring <foom@amazon.com>
2025-02-21 18:08:56 +0200

    Typo

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,6 @@
 use clap::{Args, Parser};
 #[derive(Debug, Parser)]
-#[command(version = "0.1", about = "Screenshottig app to record  my days")]
+#[command(version = "0.1", about = "Screenshotting app to record  my days")]
 pub struct AppArgs {
     ///Time in seconds between screenshots
     #[arg(short, long)]
